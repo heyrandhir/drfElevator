edad902805f06fc1a7d9dfda806b5bfa1d2d19c2
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-04-11 16:31:49 +0530

    create simple viewsets to fetch all or a particular elevator or request based on the id

--- drfElevator/elevator/views.py
+++ drfElevator/elevator/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Elevator, Request
+from .serializers import ElevatorSerializer, RequestSerializer
+
 
 # Create your views here.
+class ElevatorView(viewsets.ViewSet):
+    """
+    A simple ViewSet for all Elevators
+    """
+
+    def list(self, request):
+        queryset = Elevator.objects.all()
+        serializer = ElevatorSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk):
+        queryset = Elevator.objects.get(pk=pk)
+        serializer = ElevatorSerializer(queryset)
+        return Response(serializer.data)
+
+
+class RequestView(viewsets.ViewSet):
+    """
+    A simple ViewSet for all Requests
+    """
+
+    def list(self, request):
+        queryset = Request.objects.all()
+        serializer = RequestSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk):
+        queryset = Request.objects.get(pk=pk)
+        serializer = RequestSerializer(queryset)
+        return Response(serializer.data)
