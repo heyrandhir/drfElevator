dd2f701c5eee3a02e932bbadf89440289e7fbc68
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-04-11 15:27:44 +0530

    Create Request and Elevator Serializers

---
+++ drfElevator/elevator/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Elevator, Request
+
+
+class ElevatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Elevator
+        fields = "__all__"
+
+
+class RequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Request
+        fields = "__all__"
